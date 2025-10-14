55574111df1cf8e3e6904fc38402f1d15bb1b7b6
Govind <govind.krcrimfo555@gmail.com>
2025-10-15 04:14:51 +0530

    Abstract using Abstract class     created static method , concrete method and abstract method

---
+++ src/inheritance/abstraction/abstractEx/Ex1.java
@@ -0,0 +1,38 @@
+package inheritance.abstraction.abstractEx;
+
+/*
+ * Abstract class, different methods and how to call them
+ */
+abstract class A {
+    // static method
+    public static void method1() {
+        System.out.println("Static method ");
+    }
+
+    // concrete method
+    public void method2() {
+        System.out.println("Concrete method");
+    }
+
+    // abstract method
+    abstract void method3();
+}
+
+class B extends A {
+
+    @Override
+    void method3() {
+        System.out.println("abstract method ");
+    }
+}
+
+public class Ex1 {
+    public static void main(String[] args) {
+        A.method1(); // static method call
+        A a = new B();
+        a.method2(); // concreted method call
+        a.method3(); // normal method call ( normal method and concrete method call are same)
+
+    }
+
+}
