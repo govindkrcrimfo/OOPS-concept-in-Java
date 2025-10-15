76a93632dc10c53d39633a7109d844f83c7cb1e6
Govind <govind.krcrimfo555@gmail.com>
2025-10-16 04:06:38 +0530

    Single and multi leve inheritance code move to singleAndMulitileve package

---
+++ src/inheritance/singleAndMultilevel/Ex1.java
@@ -0,0 +1,36 @@
+package inheritance.singleAndMultilevel;
+
+/*
+* Single inheritance -> A extended by B
+* Muilti level inheritance -> A extenede by B , and B extended by C
+*/
+
+class A {
+    void greet() {
+        System.out.println("Greet from Class A");
+    }
+}
+
+class B extends A {
+    @Override
+    void greet() {
+        System.out.println("Greet from class B");
+    }
+}
+
+class C extends B {
+    @Override
+    void greet() {
+        System.out.println("Greet from class C");
+    }
+}
+
+public class Ex1 {
+    public static void main(String[] args) {
+        B b = new C();
+        b.greet();
+
+        A a = new A();
+        a.greet();
+    }
+}
