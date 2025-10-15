0cca7b7aa3d76401e93bde0a1299d9b2caf3393f
Govind <govind.krcrimfo555@gmail.com>
2025-10-16 04:09:35 +0530

    Hieararchical inheritance code move to correct package

---
+++ src/hieararchical/Ex2.java
@@ -0,0 +1,33 @@
+package hieararchical;
+
+// Hieararchical inheritance 
+
+class A {
+    void greet() {
+        System.out.println("Greet from class A");
+    }
+}
+
+class B extends A {
+    @Override
+    void greet() {
+        System.out.println("Greet from class B");
+    }
+}
+
+class C extends A {
+    @Override
+    void greet() {
+        System.out.println("Greet from class C");
+    }
+}
+
+public class Ex2 {
+    public static void main(String[] args) {
+        A a = new B();
+        a.greet();
+
+        A a1 = new C();
+        a1.greet();
+    }
+}
